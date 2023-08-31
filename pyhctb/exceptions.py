136c41b47a25a61832ab1b60d0a62834f3cfef4c
David Young <dmyoung9@asu.edu>
2023-08-31 06:22:56 -0700

    :fire: - Remove unused args from exceptions

--- pyhctb/exceptions.py
+++ pyhctb/exceptions.py
@@ -4,19 +4,19 @@
 class InvalidAuthorizationException(Exception):
     """Raised when invalid credentials are used to attempt to authenticate."""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         super().__init__("Invalid authorization!")
 
 
 class PassengerDataException(Exception):
     """Raised when passenger data cannot be parsed."""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         super().__init__("Couldn't parse passenger data!")
 
 
 class UnsuccessfulRequestException(Exception):
     """Raised when a connection cannot be established."""
 
-    def __init__(self, status_code, *args, **kwargs):
+    def __init__(self, status_code):
         super().__init__(f"Request unsuccessful: {status_code}")
